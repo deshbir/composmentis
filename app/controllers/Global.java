a5d5428b28f77a58dc557440016d64943efc4702
Deshbir Dugal <deshbir.dugal@me.com>
2012-05-07 17:21:17 +0530

    Basic Authentication added.

--- app/controllers/Global.java
+++ app/controllers/Global.java
@@ -12,9 +12,17 @@ public class Global extends Controller {
 
     @Before
     static void globalBeforeInterceptor() {
+        //Basic Auth
+        String password = request.password;
+        if ( password == null ) {
+            unauthorized( "Compro Technologies DEMO site" );
+        }
+        else if (!password.equalsIgnoreCase("play"))
+        {
+            unauthorized( "Compro Technologies DEMO site" );
+        }
 
         //Get Language Selection
-
         String lang = request.params.get("lang");
         if(lang!=null)  {
             Lang.set(lang);
