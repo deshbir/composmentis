1ec849b3d5a91e4b4ed5bc22901d9c2eb84bbaa7
Deshbir Dugal <deshbir.dugal@me.com>
2012-05-07 16:15:09 +0530

    Added H2 mem db on load.

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -9,9 +9,9 @@ public class Bootstrap extends Job {
 
     public void doJob() {
         // Check if the database is empty
-  //      if(User.count() == 0) {
-  //          Fixtures.loadModels("initial-data.yml");
-  //      }
+        if(User.count() == 0) {
+            Fixtures.loadModels("initial-data.yml");
+        }
     }
 
 }
