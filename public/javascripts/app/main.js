c428236c788b1c20a1adc338517dbf131c7ca1ec
Deshbir Dugal <deshbir.dugal@me.com>
2012-05-24 14:27:14 +0530

    Splash page customization

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
 
         render:function () {
 
-            TemplateCache.templateManager.get('ngldemo/index', function(template){
-                $("#backbone_container").html( template );
+            TemplateCache.templateManager.get('ngldemo/index', function (template) {
+                $("#backbone_container").html(template);
             });
 
             //$("#container").html(this.template);
@@ -29,8 +29,8 @@ $(document).ready(function () {
 
         render:function () {
 
-            TemplateCache.templateManager.get('ngldemo/login', function(template){
-                $("#backbone_container").html( template );
+            TemplateCache.templateManager.get('ngldemo/login', function (template) {
+                $("#backbone_container").html(template);
             });
 
             //$("#container").html(this.template);
@@ -48,8 +48,8 @@ $(document).ready(function () {
 
         render:function () {
 
-            TemplateCache.templateManager.get('ngldemo/splash', function(template){
-                $("#backbone_container").html( template );
+            TemplateCache.templateManager.get('ngldemo/splash', function (template) {
+                $("#backbone_container").html(template);
             });
 
             //$("#container").html(this.template);
@@ -67,8 +67,8 @@ $(document).ready(function () {
 
         render:function () {
 
-            TemplateCache.templateManager.get('ngldemo/home', function(template){
-                $("#backbone_container").html( template );
+            TemplateCache.templateManager.get('ngldemo/home', function (template) {
+                $("#backbone_container").html(template);
             });
 
             //$("#container").html(this.template);
@@ -86,19 +86,19 @@ $(document).ready(function () {
 
         render:function () {
 
-            TemplateCache.templateManager.get('ngldemo/launch_activity', function(template){
-                $("#backbone_container").html( template );
+            TemplateCache.templateManager.get('ngldemo/launch_activity', function (template) {
+                $("#backbone_container").html(template);
             });
 
             $LAB.setOptions({AlwaysPreserveOrder:true})
                 .script(['/public/javascripts/activityengine/kinetic-v3.9.3.js',
-                         '/public/javascripts/activityengine/raf.js',
-                         '/public/javascripts/activityengine/animate.js',
-                         '/public/javascripts/activityengine/scroller.js',
-                         '/public/javascripts/activityengine/easyScroller.js',
-                         '/public/javascripts/activityengine/container.js',
-                         '/public/javascripts/activityengine/activity.js',
-                         '/public/javascripts/activityengine/activity-engine-init.js']).wait(function() {
+                '/public/javascripts/activityengine/raf.js',
+                '/public/javascripts/activityengine/animate.js',
+                '/public/javascripts/activityengine/scroller.js',
+                '/public/javascripts/activityengine/easyScroller.js',
+                '/public/javascripts/activityengine/container.js',
+                '/public/javascripts/activityengine/activity.js',
+                '/public/javascripts/activityengine/activity-engine-init.js']).wait(function () {
                     ActivityEngineInit.init();
                 });
 
@@ -147,37 +147,38 @@ $(document).ready(function () {
     var app_router = new AppRouter;
     Backbone.history.start();
 
-    if(window.location.href.indexOf("#") == -1) {
-        app_router.navigate("ngldemo/splash",{trigger:true});
+    if (window.location.href.indexOf("#") == -1) {
+        app_router.navigate("ngldemo/splash", {trigger:true});
     }
 
     $.ajaxSetup({
-        global: true
+        global:true
     });
 
-    $(document).ajaxStart(function(){
+    $(document).ajaxStart(function () {
         $("#loadingIcon").show();
-    }) .ajaxStop(function(){
-        $("#loadingIcon").hide();
-    }).ajaxError(function(event, request, settings, error) {
+    }).ajaxStop(
+        function () {
+            $("#loadingIcon").hide();
+        }).ajaxError(function (event, request, settings, error) {
 
-        $("#error-modal .modal-body").html(request.responseText);
-        $("#error-modal .error-status-code").html("Status code:" + request.status);
-        $("#error-modal .error-message").html("Error Message:" + error);
-        $("#error-modal .error-url").html("URL:" + settings.url);
+            $("#error-modal .modal-body").html(request.responseText);
+            $("#error-modal .error-status-code").html("Status code:" + request.status);
+            $("#error-modal .error-message").html("Error Message:" + error);
+            $("#error-modal .error-url").html("URL:" + settings.url);
 
-        //launch error modal dialog such that it centers itself, and width is based on the content.
-        $('#error-modal').modal({
-            backdrop: true,
-            keyboard: true
-        }).css({
-                width: 'auto',
-                'margin-left': function () {
-                    return -($(this).width() / 2);
-                }
+            //launch error modal dialog such that it centers itself, and width is based on the content.
+            $('#error-modal').modal({
+                backdrop:true,
+                keyboard:true
+            }).css({
+                    width:'auto',
+                    'margin-left':function () {
+                        return -($(this).width() / 2);
+                    }
+                });
+            //app_router.navigate("ngldemo/login",{trigger:true});
+            window.history.back();
         });
-        //app_router.navigate("ngldemo/login",{trigger:true});
-        window.history.back();
-    });
 
 });
